1b59023b7bb95d4c9e70deff55e78b4c8ecd237c
Daniel_J Carig <24857822+dannnnnn04@users.noreply.github.com>
2023-12-09 12:38:56 -0800

    Add function that plays a single round of R,P,S
    "playRound": this function takes two parameters (playerSelection, computerSelection) - and then return a string that declares the winner of the round

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 function getComputerChoice() {
-    const choices = [ "Rock","Paper","Scissors"];
-    return Math.floor(Math.random() * choices.length);
+    const choices = [ "rock","paper","scissor"];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+    console.log(`playerSelection: ${playerSelection}, computerSelection: ${computerSelection}`);
+    let TIE = true;
+    playerSelection = playerSelection.toLowerCase();
+    
+    while (TIE){
+        if (playerSelection === computerSelection) {
+            return "It's a tie!";
+        }
+        else if (playerSelection === "rock") {
+            if (computerSelection === "paper") {
+                return "You Lose! Paper beats Rock";
+            }
+            else if (computerSelection === "scissor") {
+                return "You Win! Rock beats Scissor";
+            }
+        }
+        else if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                return "You Win! Paper beats Rock";
+            }
+            else if (computerSelection === "scissor") {
+                return "You Lose! Scissor beats Paper";
+            }
+        }
+        else if (playerSelection === "scissor") {
+            if (computerSelection === "rock") {
+                return "You Lose! Rock beats Scissor";
+            }
+            else if (computerSelection === "paper") {
+                return "You Win! Scissor beats Paper";
+            }
+        }
+        TIE = false;
+    }
+}
+
+const playerSelection = "rock";
+// const playerSelection = "paper";
+// const playerSelection = "scissor";
+
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
